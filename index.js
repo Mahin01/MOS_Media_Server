58d7210d07f6d35e65de321504add061c27f14c6
Mahin01 <Ashmahin4@gmail.com>
2023-06-09 16:31:27 +0600

    Backend API created for inserting registered user data

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("mos-media");
+    const usersCollection = database.collection("users");
+
+    // Insert Registered User Data 
+    app.post("/users", async(req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
