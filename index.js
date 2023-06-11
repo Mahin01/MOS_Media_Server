b53b025d028934ab17dd64200e41826321e2980b
Mahin01 <Ashmahin4@gmail.com>
2023-06-11 15:11:37 +0600

    Error Fixed on selected class delete API

--- index.js
+++ index.js
@@ -56,11 +56,9 @@ async function run() {
     app.delete("/selected-class/:id", async(req, res) => {
       const id = req.params.id;
       const query = { _id : new ObjectId(id)};
-      const result = selectedClassesCollection.deleteOne(query);
+      const result = await selectedClassesCollection.deleteOne(query);
       res.send(result);
     })
-
-
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
