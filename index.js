f2a9a5a77e0e754bf4faefd6b989ebaa762858d8
Mahin01 <Ashmahin4@gmail.com>
2023-06-11 16:50:49 +0600

    Delete user api error fixed

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function run() {
     app.delete("/users/:id", async(req, res) => {
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
-      const result = usersCollection.deleteOne(query);
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     })
 
